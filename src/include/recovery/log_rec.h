141052266ed136fb9b000e7834a0ba1bf9063ba1
lvsooya <3240104053@zju.edu.cn>
2026-06-14 18:49:47 +0800

    recovery代码完成

--- src/include/recovery/log_rec.h
+++ src/include/recovery/log_rec.h
@@ -1,6 +1,7 @@
 #ifndef MINISQL_LOG_REC_H
 #define MINISQL_LOG_REC_H
 
+#include <memory>
 #include <unordered_map>
 #include <utility>
 
@@ -23,7 +24,8 @@ using KeyType = std::string;
 using ValType = int32_t;
 
 /**
- * TODO: Student Implement
+ * LogRec 表示内存 WAL 中的一条日志记录。
+ * 包含所有日志类型可能用到的字段；对于特定类型，未使用的字段保持默认值。
  */
 struct LogRec {
     LogRec() = default;
@@ -31,8 +33,14 @@ struct LogRec {
     LogRecType type_{LogRecType::kInvalid};
     lsn_t lsn_{INVALID_LSN};
     lsn_t prev_lsn_{INVALID_LSN};
+    txn_id_t txn_id_{INVALID_TXN_ID};
 
-    /* used for testing only */
+    /* 数据操作相关字段 */
+    KeyType key_{};
+    ValType old_val_{0};
+    ValType new_val_{0};
+
+    /* 仅供测试使用 */
     static std::unordered_map<txn_id_t, lsn_t> prev_lsn_map_;
     static lsn_t next_lsn_;
 };
@@ -42,46 +50,76 @@ lsn_t LogRec::next_lsn_ = 0;
 
 typedef std::shared_ptr<LogRec> LogRecPtr;
 
-/**
- * TODO: Student Implement
- */
 static LogRecPtr CreateInsertLog(txn_id_t txn_id, KeyType ins_key, ValType ins_val) {
-    return nullptr;
+    auto log = std::make_shared<LogRec>();
+    log->type_ = LogRecType::kInsert;
+    log->txn_id_ = txn_id;
+    log->key_ = std::move(ins_key);
+    log->new_val_ = ins_val;
+    log->lsn_ = LogRec::next_lsn_++;
+    auto it = LogRec::prev_lsn_map_.find(txn_id);
+    log->prev_lsn_ = (it != LogRec::prev_lsn_map_.end()) ? it->second : INVALID_LSN;
+    LogRec::prev_lsn_map_[txn_id] = log->lsn_;
+    return log;
 }
 
-/**
- * TODO: Student Implement
- */
 static LogRecPtr CreateDeleteLog(txn_id_t txn_id, KeyType del_key, ValType del_val) {
-    return nullptr;
+    auto log = std::make_shared<LogRec>();
+    log->type_ = LogRecType::kDelete;
+    log->txn_id_ = txn_id;
+    log->key_ = std::move(del_key);
+    log->old_val_ = del_val;
+    log->lsn_ = LogRec::next_lsn_++;
+    auto it = LogRec::prev_lsn_map_.find(txn_id);
+    log->prev_lsn_ = (it != LogRec::prev_lsn_map_.end()) ? it->second : INVALID_LSN;
+    LogRec::prev_lsn_map_[txn_id] = log->lsn_;
+    return log;
 }
 
-/**
- * TODO: Student Implement
- */
 static LogRecPtr CreateUpdateLog(txn_id_t txn_id, KeyType old_key, ValType old_val, KeyType new_key, ValType new_val) {
-    return nullptr;
+    auto log = std::make_shared<LogRec>();
+    log->type_ = LogRecType::kUpdate;
+    log->txn_id_ = txn_id;
+    log->key_ = std::move(new_key);
+    log->old_val_ = old_val;
+    log->new_val_ = new_val;
+    log->lsn_ = LogRec::next_lsn_++;
+    auto it = LogRec::prev_lsn_map_.find(txn_id);
+    log->prev_lsn_ = (it != LogRec::prev_lsn_map_.end()) ? it->second : INVALID_LSN;
+    LogRec::prev_lsn_map_[txn_id] = log->lsn_;
+    return log;
 }
 
-/**
- * TODO: Student Implement
- */
 static LogRecPtr CreateBeginLog(txn_id_t txn_id) {
-    return nullptr;
+    auto log = std::make_shared<LogRec>();
+    log->type_ = LogRecType::kBegin;
+    log->txn_id_ = txn_id;
+    log->lsn_ = LogRec::next_lsn_++;
+    log->prev_lsn_ = INVALID_LSN;
+    LogRec::prev_lsn_map_[txn_id] = log->lsn_;
+    return log;
 }
 
-/**
- * TODO: Student Implement
- */
 static LogRecPtr CreateCommitLog(txn_id_t txn_id) {
-    return nullptr;
+    auto log = std::make_shared<LogRec>();
+    log->type_ = LogRecType::kCommit;
+    log->txn_id_ = txn_id;
+    log->lsn_ = LogRec::next_lsn_++;
+    auto it = LogRec::prev_lsn_map_.find(txn_id);
+    log->prev_lsn_ = (it != LogRec::prev_lsn_map_.end()) ? it->second : INVALID_LSN;
+    LogRec::prev_lsn_map_[txn_id] = log->lsn_;
+    return log;
 }
 
-/**
- * TODO: Student Implement
- */
 static LogRecPtr CreateAbortLog(txn_id_t txn_id) {
-    return nullptr;
+    auto log = std::make_shared<LogRec>();
+    log->type_ = LogRecType::kAbort;
+    log->txn_id_ = txn_id;
+    log->lsn_ = LogRec::next_lsn_++;
+    auto it = LogRec::prev_lsn_map_.find(txn_id);
+    log->prev_lsn_ = (it != LogRec::prev_lsn_map_.end()) ? it->second : INVALID_LSN;
+    LogRec::prev_lsn_map_[txn_id] = log->lsn_;
+    return log;
 }
 
 #endif  // MINISQL_LOG_REC_H
--- src/include/recovery/recovery_manager.h
+++ src/include/recovery/recovery_manager.h
@@ -22,32 +22,124 @@ struct CheckPoint {
 
 class RecoveryManager {
 public:
-    /**
-    * TODO: Student Implement
-    */
-    void Init(CheckPoint &last_checkpoint) {}
+    void Init(CheckPoint &last_checkpoint) {
+        persist_lsn_ = last_checkpoint.checkpoint_lsn_;
+        active_txns_ = last_checkpoint.active_txns_;
+        data_ = last_checkpoint.persist_data_;
+    }
 
-    /**
-    * TODO: Student Implement
-    */
-    void RedoPhase() {}
+    void RedoPhase() {
+        // 正向重做：从检查点之后重放所有日志记录
+        auto it = log_recs_.upper_bound(persist_lsn_);
+        for (; it != log_recs_.end(); ++it) {
+            const auto &log = it->second;
 
-    /**
-    * TODO: Student Implement
-    */
-    void UndoPhase() {}
+            switch (log->type_) {
+                case LogRecType::kBegin:
+                    active_txns_[log->txn_id_] = log->lsn_;
+                    break;
 
-    // used for test only
+                case LogRecType::kCommit:
+                    active_txns_.erase(log->txn_id_);
+                    break;
+
+                case LogRecType::kInsert:
+                    data_[log->key_] = log->new_val_;
+                    break;
+
+                case LogRecType::kDelete:
+                    data_.erase(log->key_);
+                    break;
+
+                case LogRecType::kUpdate:
+                    data_[log->key_] = log->new_val_;
+                    break;
+
+                case LogRecType::kAbort: {
+                    // 没有 CLR 日志：沿该事务的 prev_lsn 链逆序遍历，
+                    // 将其所有修改反向物理撤销
+                    lsn_t cur_lsn = log->prev_lsn_;
+                    while (cur_lsn != INVALID_LSN) {
+                        auto undo_it = log_recs_.find(cur_lsn);
+                        if (undo_it == log_recs_.end()) break;
+                        const auto &undo_log = undo_it->second;
+                        if (undo_log->txn_id_ != log->txn_id_) break;
+
+                        switch (undo_log->type_) {
+                            case LogRecType::kInsert:
+                                // 撤销插入：删除 key
+                                data_.erase(undo_log->key_);
+                                break;
+                            case LogRecType::kDelete:
+                                // 撤销删除：恢复 old_val
+                                data_[undo_log->key_] = undo_log->old_val_;
+                                break;
+                            case LogRecType::kUpdate:
+                                // 撤销更新：恢复 old_val
+                                data_[undo_log->key_] = undo_log->old_val_;
+                                break;
+                            default:
+                                break;
+                        }
+                        cur_lsn = undo_log->prev_lsn_;
+                    }
+                    active_txns_.erase(log->txn_id_);
+                    break;
+                }
+
+                default:
+                    break;
+            }
+        }
+    }
+
+    void UndoPhase() {
+        // 反向撤销：逆序遍历日志，撤销仍活跃事务的所有操作
+        for (auto it = log_recs_.rbegin(); it != log_recs_.rend(); ++it) {
+            const auto &log = it->second;
+
+            if (active_txns_.find(log->txn_id_) == active_txns_.end()) {
+                continue;
+            }
+
+            switch (log->type_) {
+                case LogRecType::kInsert:
+                    // 撤销插入：删除 key
+                    data_.erase(log->key_);
+                    break;
+
+                case LogRecType::kDelete:
+                    // 撤销删除：恢复 old_val
+                    data_[log->key_] = log->old_val_;
+                    break;
+
+                case LogRecType::kUpdate:
+                    // 撤销更新：恢复 old_val
+                    data_[log->key_] = log->old_val_;
+                    break;
+
+                case LogRecType::kBegin:
+                    // 遇到未提交事务的 BEGIN，从活跃事务表中移除
+                    active_txns_.erase(log->txn_id_);
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+
+    // 仅供测试使用
     void AppendLogRec(LogRecPtr log_rec) { log_recs_.emplace(log_rec->lsn_, log_rec); }
 
-    // used for test only
+    // 仅供测试使用
     inline KvDatabase &GetDatabase() { return data_; }
 
 private:
     std::map<lsn_t, LogRecPtr> log_recs_{};
     lsn_t persist_lsn_{INVALID_LSN};
     ATT active_txns_{};
-    KvDatabase data_{};  // all data in database
+    KvDatabase data_{};  // 数据库中所有数据
 };
 
 #endif  // MINISQL_RECOVERY_MANAGER_H
